2a742ae12036b5b208d6373bb21b0ed18fb75bcb
bhavya <bhavyajain817@gmail.com>
2022-09-13 17:16:32 +0530

    Updates

---
+++ precache-manifest.8beff7a3fa2a95d0f0c4f748526f0d90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dd49f096cfd6f1e99aa",
+    "url": "/WeatherEx/static/css/main.2e147004.chunk.css"
+  },
+  {
+    "revision": "2dd49f096cfd6f1e99aa",
+    "url": "/WeatherEx/static/js/main.2dd49f09.chunk.js"
+  },
+  {
+    "revision": "f993ed0ea40465f97e7e",
+    "url": "/WeatherEx/static/js/1.f993ed0e.chunk.js"
+  },
+  {
+    "revision": "729c9a921b55310086c9",
+    "url": "/WeatherEx/static/js/2.729c9a92.chunk.js"
+  },
+  {
+    "revision": "ad01e7662159ca091c81",
+    "url": "/WeatherEx/static/js/runtime~main.ad01e766.js"
+  },
+  {
+    "revision": "fa8b05e9a54cbf89b664d56a5efe04d3",
+    "url": "/WeatherEx/static/media/weather-images.fa8b05e9.jpg"
+  },
+  {
+    "revision": "c25a5fbd2f04049c2aa5eac11d44bcca",
+    "url": "/WeatherEx/index.html"
+  }
+];
